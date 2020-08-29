4f9fed45ceae3d30526bf9df5ce647643a064517
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2020-08-29 13:19:51 +0200

    Deleted Superflous files
    To clean up repository, held old code

--- Svector.jl
+++
@@ -1,9 +0,0 @@
-using StaticArrays
-
-function mv(arr::Vector{T}, ::Val{N}) where {N,T}
-       m=MVector{N,T}(undef)
-       for i=1:N
-       @inbounds m[i]=arr[i]
-       end
-       return SVector(m)
-end
--- TestFile.jl
+++
@@ -1,5 +0,0 @@
-#File holding some older tests
-
-#Motion
-#Pos = PostSPH.readVtkArray(filename,PostSPH.Points)
-ace = PostSPH.readVtkArray("PartFloating",PostSPH.Ace)
