2179d0e0a3969cbb5440c486f0ff5803eb020a64
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2021-08-21 00:37:26 +0200

    Final edition with fasted pressure evaluation

--- Evaluate_Pressure.jl
+++ Evaluate_Pressure.jl
@@ -2,45 +2,21 @@
 using BenchmarkTools
 using LoopVectorization
 rho   = rand(990:rand():1010,10^3)
+P     = similar(rho)
 
-function EOS1(rho;c0=100,gamma=7,rho0=1000)
-    b=(c0^2*rho0)/gamma
-
-    P     = b*((rho/rho0).^7 .- 1)
+function EOSSimple(rho;c0=100.0,rho0=1000.0)
+    return  c0^2*(rho-rho0)
 end
 
-function EOS1SIMILAR(rho;c0=100,gamma=7,rho0=1000)
+
+function EOS(rho;c0=100.0,gamma=7,rho0=1000.0)
     b=(c0^2*rho0)/gamma
-
-    P     = similar(rho)
-
-    P     .= b*((rho/rho0).^7 .- 1)
+    return  b*((rho/rho0)^gamma - 1.0)
 end
 
-function EOS2(rho;c0=100,gamma=7,rho0=1000)
-    b=(c0^2*rho0)/gamma
-
-    rho0_denominator = 1/(rho0^7);
-
-    P     = b*(rho.^7 * rho0_denominator .- 1)
+function evaluateFunc!(func,array,arr)
+    @tturbo @. arr = func(array)
 end
 
-function EOS3(rho;c0=100,gamma=7,rho0=1000)
-    b=(c0^2*rho0)/gamma
-
-    rho0_denominator = 1/(rho0^7);
-
-    P     = rho.^7 * rho0_denominator * b .- b
-end
-
-bEOS1  = @benchmark EOS1($rho)
-println("Minimum Time EOS1:\n",bEOS1)
-bEOS2  = @benchmark EOS2($rho)
-println("Minimum Time EOS2:\n",bEOS2)
-bEOS3  = @benchmark EOS3($rho)
-println("Minimum Time EOS3:\n",bEOS3)
-
-bEOS1AVX  = @benchmark @avx EOS1($rho)
-println("Minimum Time EOS1AVX:\n",bEOS1AVX)
-bEOS1SIMILAR  = @benchmark EOS1SIMILAR($rho)
-println("Minimum Time EOS1SIMILAR:\n",bEOS1SIMILAR)
+@benchmark evaluateFunc!($EOSSimple,$rho,$P)
+@benchmark evaluateFunc!($EOS,$rho,$P)
