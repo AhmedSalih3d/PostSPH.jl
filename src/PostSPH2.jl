0f765dc21eb39a7199079abd3fd6a49fb52d7de1
Saif Salih <saifsalih5@gmail.com>
2021-08-28 12:46:47 +0200

    Fixed formatting with JuliaFormatter.jl

--- src/PostSPH2.jl
+++ src/PostSPH2.jl
@@ -7,8 +7,7 @@ using StaticArrays
 
 include("..SaveVTK2.jl")
 
-export
-    readVtkArray,
+export readVtkArray,
     Cat,
     readVtkParticles,
     ForceVtk,
@@ -21,63 +20,67 @@ export
     MkArray
 
 ##Hardcoded enum - Cat is "category"
-        @enum Cat begin
-            Points
-            Idp
-            Vel
-            Rhop
-            Mass
-            Press
-            Vol
-            Ace
-            Vor
-            Typ #Typ, since "Type" is illegal to assign
-            Mk
-        end
+@enum Cat begin
+    Points
+    Idp
+    Vel
+    Rhop
+    Mass
+    Press
+    Vol
+    Ace
+    Vor
+    Typ #Typ, since "Type" is illegal to assign
+    Mk
+end
 
 ##Read from binary files directly
 
 ## Make two dicts,   one for the enum -> string and one for the data type - Bi4 specific
-const varNames  = (:key, :offset)
+const varNames = (:key, :offset)
 
-const IdpSearch    = [0x41;0x52;0x52;0x41;0x59;0x03;0x00;0x00;0x00;0x49;0x64;0x70]
-const IdpOffset    = 11
-const IdpKey       = NamedTuple{varNames}([IdpSearch,IdpOffset])
+const IdpSearch = [0x41; 0x52; 0x52; 0x41; 0x59; 0x03; 0x00; 0x00; 0x00; 0x49; 0x64; 0x70]
+const IdpOffset = 11
+const IdpKey = NamedTuple{varNames}([IdpSearch, IdpOffset])
 
-const PosSearch    = [0x41;0x52;0x52;0x41;0x59;0x03;0x00;0x00;0x00;0x50;0x6f;0x73]
-const PosOffset    = 11
-const PosKey       = NamedTuple{varNames}([PosSearch,PosOffset])
+const PosSearch = [0x41; 0x52; 0x52; 0x41; 0x59; 0x03; 0x00; 0x00; 0x00; 0x50; 0x6f; 0x73]
+const PosOffset = 11
+const PosKey = NamedTuple{varNames}([PosSearch, PosOffset])
 
-const VelSearch    = [0x41;0x52;0x52;0x41;0x59;0x03;0x00;0x00;0x00;0x56;0x65;0x6c]
-const VelOffset    = 11
-const VelKey       = NamedTuple{varNames}([VelSearch,VelOffset])
+const VelSearch = [0x41; 0x52; 0x52; 0x41; 0x59; 0x03; 0x00; 0x00; 0x00; 0x56; 0x65; 0x6c]
+const VelOffset = 11
+const VelKey = NamedTuple{varNames}([VelSearch, VelOffset])
 
-const RhopSearch   = [0x41;0x52;0x52;0x41;0x59;0x04;0x00;0x00;0x00;0x52;0x68;0x6f;0x70]
-const RhopOffset   = 12
-const RhopKey      = NamedTuple{varNames}([RhopSearch,RhopOffset])
+const RhopSearch =
+    [0x41; 0x52; 0x52; 0x41; 0x59; 0x04; 0x00; 0x00; 0x00; 0x52; 0x68; 0x6f; 0x70]
+const RhopOffset = 12
+const RhopKey = NamedTuple{varNames}([RhopSearch, RhopOffset])
 
-const searchKeyBi4    = Dict{Cat,NamedTuple}(Idp => IdpKey, Points => PosKey, Vel => VelKey, Rhop => RhopKey)
-const catTypeBi4 = Dict{Cat,DataType}(Idp => Int32, Points => Float32, Vel => Float32, Rhop => Float32)
-const catArrayBi4  = Dict{Cat,Int64}(Idp => 1, Points => 2, Vel => 2, Rhop => 1)
-const catColBi4  = Dict{Cat,Int64}(Idp => 1, Points => 3, Vel => 3, Rhop => 1)
+const searchKeyBi4 =
+    Dict{Cat,NamedTuple}(Idp => IdpKey, Points => PosKey, Vel => VelKey, Rhop => RhopKey)
+const catTypeBi4 =
+    Dict{Cat,DataType}(Idp => Int32, Points => Float32, Vel => Float32, Rhop => Float32)
+const catArrayBi4 = Dict{Cat,Int64}(Idp => 1, Points => 2, Vel => 2, Rhop => 1)
+const catColBi4 = Dict{Cat,Int64}(Idp => 1, Points => 3, Vel => 3, Rhop => 1)
 
 ##Lists files in directory and only returns applicable files, ie. "Part_XXXX.bi4"
-function _dirFiles(first_file::Bool=false)
+function _dirFiles(first_file::Bool = false)
     if first_file == false
         files = readdir()
     else
         files = readdir()[1]
     end
     #Operation on dirFiles instantly
-    filter!(x->occursin(r"Part_\d{4}.bi4",x),files)
+    filter!(x -> occursin(r"Part_\d{4}.bi4", x), files)
     return files
 end
 
 ##transferData is smart since we change the value of an array in place and
 # skip a lot of unnecessary allocation steps. It has been tuned to accept
 # matrices and vectors, where the type is automatically deduced using "eltype".
- _readel(ft::IOStream, typ) = read(ft, typ)
-@inline _readel(ft::IOStream, typ::Type{<:SVector{3,T}}) where T = SVector(_readel(ft, T), _readel(ft, T), _readel(ft, T))
+_readel(ft::IOStream, typ) = read(ft, typ)
+@inline _readel(ft::IOStream, typ::Type{<:SVector{3,T}}) where {T} =
+    SVector(_readel(ft, T), _readel(ft, T), _readel(ft, T))
 
 function _transferDataBi4(ft::IOStream, arrayVal::AbstractVector)
     typ = eltype(arrayVal)
@@ -88,38 +91,43 @@ end
 
 
 ##If nCol is not 1, then type is static array
-_typeMaker(typ, nCol) = nCol == 1 ? typ : SVector{nCol, typ}
+_typeMaker(typ, nCol) = nCol == 1 ? typ : SVector{nCol,typ}
 
 #Command for plotting
 #println(Char.(key))
 #scatter(getindex.(a[1], 1), getindex.(a[1], 3))
 #readBi4Array(typ::Cat,Bi4Files::Array{String,1}=_dirFiles()) = readBi4Array(typ, false, Bi4Files)
-readBi4Array(typ::Cat,Bi4Files::Array{String,1}) = readBi4Array(typ, false, Bi4Files)
-readBi4Array(typ::Cat,SeekNull::Bool,Bi4Files::String) = readBi4Array(typ, false, [Bi4Files])
-readBi4Array(typ::Cat,Bi4Files::String) = readBi4Array(typ, false, [Bi4Files])
-function readBi4Array(typ::Cat,SeekNull::Bool=false,Bi4Files::Array{String,1}=_dirFiles())
+readBi4Array(typ::Cat, Bi4Files::Array{String,1}) = readBi4Array(typ, false, Bi4Files)
+readBi4Array(typ::Cat, SeekNull::Bool, Bi4Files::String) =
+    readBi4Array(typ, false, [Bi4Files])
+readBi4Array(typ::Cat, Bi4Files::String) = readBi4Array(typ, false, [Bi4Files])
+function readBi4Array(
+    typ::Cat,
+    SeekNull::Bool = false,
+    Bi4Files::Array{String,1} = _dirFiles(),
+)
 
     #if true start from top, else do not, use startPos from _Bi4Pos
-    nBi4     = size(Bi4Files)[1]
+    nBi4 = size(Bi4Files)[1]
 
-    T  = _typeMaker(catTypeBi4[typ], catColBi4[typ])
-    j  = fill(Array{T,1}(), nBi4) #Less allocs than Vector{Array{T}}(undef,nBi4)
+    T = _typeMaker(catTypeBi4[typ], catColBi4[typ])
+    j = fill(Array{T,1}(), nBi4) #Less allocs than Vector{Array{T}}(undef,nBi4)
 
-    key    = searchKeyBi4[typ].key
+    key = searchKeyBi4[typ].key
     offset = searchKeyBi4[typ].offset
 
     Threads.@threads for i = 1:nBi4
-        ft = open(Bi4Files[i],read=true)
+        ft = open(Bi4Files[i], read = true)
         rf = read(ft)
         startPos = Base._searchindex(rf, key, 1) + offset #1 byte offset
-        seek(ft,startPos)
+        seek(ft, startPos)
 
-            read(ft,Int64)
-        n = read(ft,Int32)
-            read(ft,Int32)
+        read(ft, Int64)
+        n = read(ft, Int32)
+        read(ft, Int32)
 
-            j[i] = zeros(T,n)
-            _transferDataBi4(ft,j[i])
+        j[i] = zeros(T, n)
+        _transferDataBi4(ft, j[i])
 
 
         close(ft)
@@ -129,20 +137,20 @@ end
 
 ##StaticArrays is hard to use here since it is needed to offset with "Int32", between
 # all searches unlike "readBi4Array"
-function readBi4Particles(Bi4Files::Array{String,1}=_dirFiles())
+function readBi4Particles(Bi4Files::Array{String,1} = _dirFiles())
 
-    nBi4     = size(Bi4Files)[1]
+    nBi4 = size(Bi4Files)[1]
 
-    j  = Vector{Array{Int32,1}}(undef,nBi4)
+    j = Vector{Array{Int32,1}}(undef, nBi4)
 
-    ParticleString = ["CaseNp","CaseNfixed","CaseNmoving","CaseNfloat","CaseNfluid"]
+    ParticleString = ["CaseNp", "CaseNfixed", "CaseNmoving", "CaseNfloat", "CaseNfluid"]
     Threads.@threads for i = 1:nBi4
-        ft = open(Bi4Files[i],read=true)
-        j_inner = zeros(Int32,5)
+        ft = open(Bi4Files[i], read = true)
+        j_inner = zeros(Int32, 5)
         for k in eachindex(j_inner)
-            readuntil(ft,ParticleString[k])
-            read(ft,Int32)
-            j_inner[k] = read(ft,Int32)
+            readuntil(ft, ParticleString[k])
+            read(ft, Int32)
+            j_inner[k] = read(ft, Int32)
         end
         j[i] = j_inner
         close(ft)
@@ -151,18 +159,18 @@ function readBi4Particles(Bi4Files::Array{String,1}=_dirFiles())
 end
 
 ## Function to read the time at current simulation step, as in "XXXX.out"
-function readBi4Time(Bi4Files::Array{String,1}=_dirFiles())
-    nBi4     = size(Bi4Files)[1]
+function readBi4Time(Bi4Files::Array{String,1} = _dirFiles())
+    nBi4 = size(Bi4Files)[1]
 
-    T  = Float64
-    j  = Vector{T}(undef,nBi4)
+    T = Float64
+    j = Vector{T}(undef, nBi4)
 
     ParticleString = ["TimeStep"]
     Threads.@threads for i = 1:nBi4
-        ft = open(Bi4Files[i],read=true)
-        readuntil(ft,ParticleString[1])
-        read(ft,Int32)
-        j[i] = read(ft,T)
+        ft = open(Bi4Files[i], read = true)
+        readuntil(ft, ParticleString[1])
+        read(ft, Int32)
+        j[i] = read(ft, T)
         close(ft)
     end
     return j
@@ -173,12 +181,12 @@ end
 #the second index is the sorting of this "typ" array corresponding to the "idp"
 #array and "start:move" are the number of particles defined by the "Body", where
 #0 and 1 indexing from C++ and Julia differences has been taken into account.
-readBi4Body(Body,typ,Bi4Files::String) =  readBi4Body(Body, typ, [Bi4Files])
-function readBi4Body(Body,typ,Bi4Files::Array{String,1}=_dirFiles())
-    start = getfield(Body, :beg)+1    #First idp
-    move  = start+getfield(Body, :count)-1  #Number of particles from first idp
-    idp_vec  = readBi4Array(Idp,false,Bi4Files)
-    val_vec  = readBi4Array(typ,false,Bi4Files)
+readBi4Body(Body, typ, Bi4Files::String) = readBi4Body(Body, typ, [Bi4Files])
+function readBi4Body(Body, typ, Bi4Files::Array{String,1} = _dirFiles())
+    start = getfield(Body, :beg) + 1    #First idp
+    move = start + getfield(Body, :count) - 1  #Number of particles from first idp
+    idp_vec = readBi4Array(Idp, false, Bi4Files)
+    val_vec = readBi4Array(typ, false, Bi4Files)
 
     j = similar(val_vec)
 
--- src/SaveVTK.jl
+++ src/SaveVTK.jl
@@ -1,62 +1,61 @@
 module SaveVTK
 
-    using StaticArrays
-    using WriteVTK
-    using Random
-    using Parameters
+using StaticArrays
+using WriteVTK
+using Random
+using Parameters
 
-    export
-        write_vtp
+export write_vtp
 
-    """
-        Struct to hold simulation data
-    """
-    @with_kw mutable struct SimData
-        Points#::Array{Float32,1} = Array{Float32}(undef, 0)
-        Idp#::Array{Int32,1} = Array{Int32,1}()
-        Vel#::Array{Float32,1} = Array{Float32}(undef, 0)
-        Rhop#::Array{Float32,1} = Array{Float32,1}()
+"""
+    Struct to hold simulation data
+"""
+@with_kw mutable struct SimData
+    Points::Any#::Array{Float32,1} = Array{Float32}(undef, 0)
+    Idp::Any#::Array{Int32,1} = Array{Int32,1}()
+    Vel::Any#::Array{Float32,1} = Array{Float32}(undef, 0)
+    Rhop::Any#::Array{Float32,1} = Array{Float32,1}()
+end
+
+"""
+    write_vtp(filename,points,attribute)
+
+Saves a polydata file (.vtp) given a filename, a 2xN point array and and 1xN attribute array.
+"""
+function write_vtp(filename::String, sim_arr::SimData)
+
+    x = sim_arr.Points[1:3:end]
+    y = sim_arr.Points[2:3:end]
+    z = sim_arr.Points[3:3:end]
+
+    points = hcat(x, y, z)'
+
+    polys = empty(MeshCell{WriteVTK.PolyData.Polys,UnitRange{Int64}}[])
+    verts = empty(MeshCell{WriteVTK.PolyData.Verts,UnitRange{Int64}}[])
+
+    # Note: the order of verts, lines, polys and strips is not important.
+    # One doesn't even need to pass all of them.
+    all_cells = (verts, polys)
+
+    #allocations here, not my library
+    vtk = vtk_grid(filename, points, all_cells..., compress = true, append = false)
+
+
+    if !isempty(sim_arr.Idp)
+        vtk["Idp"] = sim_arr.Idp
+    end
+    if !isempty(sim_arr.Vel)
+        vx = sim_arr.Vel[1:3:end]
+        vy = sim_arr.Vel[2:3:end]
+        vz = sim_arr.Vel[3:3:end]
+        velocity = hcat(vx, vy, vz)'
+        vtk["Velocity"] = velocity
+    end
+    if !isempty(sim_arr.Rhop)
+        vtk["Rhop"] = sim_arr.Rhop
     end
 
-    """
-        write_vtp(filename,points,attribute)
-
-    Saves a polydata file (.vtp) given a filename, a 2xN point array and and 1xN attribute array.
-    """
-    function write_vtp(filename::String, sim_arr::SimData)
-
-        x        = sim_arr.Points[1:3:end]
-        y        = sim_arr.Points[2:3:end]
-        z        = sim_arr.Points[3:3:end]
-
-        points   = hcat(x,y,z)'
-
-        polys = empty(MeshCell{WriteVTK.PolyData.Polys,UnitRange{Int64}}[])
-        verts = empty(MeshCell{WriteVTK.PolyData.Verts,UnitRange{Int64}}[])
-
-        # Note: the order of verts, lines, polys and strips is not important.
-        # One doesn't even need to pass all of them.
-        all_cells = (verts, polys)
-
-        #allocations here, not my library
-        vtk  = vtk_grid(filename, points, all_cells..., compress=true, append=false)
-
-
-        if !isempty(sim_arr.Idp)
-            vtk["Idp"]      = sim_arr.Idp
-        end
-        if !isempty(sim_arr.Vel)
-            vx       = sim_arr.Vel[1:3:end]
-            vy       = sim_arr.Vel[2:3:end]
-            vz       = sim_arr.Vel[3:3:end]
-            velocity = hcat(vx,vy,vz)'
-            vtk["Velocity"] = velocity
-        end
-        if !isempty(sim_arr.Rhop)
-            vtk["Rhop"]     = sim_arr.Rhop
-        end
-
-        vtk_save(vtk)
-    end
+    vtk_save(vtk)
+end
 
 end
