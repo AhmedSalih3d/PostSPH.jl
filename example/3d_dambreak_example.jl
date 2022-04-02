b2e09ddea9e8fb5178bceba6092060af55b928de
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2022-04-02 15:26:46 +0200

    Think I got it, use PostSPH.SaveVTK

--- example/3d_dambreak_example.jl
+++ example/3d_dambreak_example.jl
@@ -1,4 +1,4 @@
-#using PostSPH
+using PostSPH
 
 # Load all data in
 cd(raw"C:\Users\Ahmed Salih\Documents\DualSPHysics_v5.0\examples\main\01_DamBreak\CaseDambreak_out\data")
@@ -23,14 +23,14 @@ function SaveVTK_out(pos_array,idp_array,vel_array,rhop_array)
         Fluid_act_id    = findall(Fluid_Ids["IdRangeJulia"][1]  .<= idp_array[i] .<= Fluid_Ids["IdRangeJulia"][end])  #filter did not work?
         Column_act_id   = findall(Column_Ids["IdRangeJulia"][1] .<= idp_array[i] .<= Column_Ids["IdRangeJulia"][end]) #filter(x -> Column_Ids["IdRangeJulia"][1] <= x < Column_Ids["IdRangeJulia"][end], idp_array[i])
 
-        SimDataFluid    = SaveVTK.SimData(
+        SimDataFluid    = PostSPH.SaveVTK.SimData(
             Points = pos_array[i],
             Idp    = idp_array[i],
             Vel    = vel_array[i],
             Rhop   = rhop_array[i],
         )
 
-        SimDataColumn   = SaveVTK.SimData(
+        SimDataColumn   = PostSPH.SaveVTK.SimData(
             Points = pos_array[i],
             Idp    = idp_array[i],
             Vel    = vel_array[i],
@@ -38,8 +38,8 @@ function SaveVTK_out(pos_array,idp_array,vel_array,rhop_array)
         )
 
         #Use 3d glyph filter in Paraview with glyphs!
-        SaveVTK.write_vtp("SimData_FLUID_" * lpad(string(i), 4, "0"), SimDataFluid,Fluid_act_id)
-        SaveVTK.write_vtp("SimData_COLUMN_" * lpad(string(i), 4, "0"), SimDataColumn,Column_act_id)
+        PostSPH.SaveVTK.write_vtp("SimData_FLUID_" * lpad(string(i), 4, "0"), SimDataFluid,Fluid_act_id)
+        PostSPH.SaveVTK.write_vtp("SimData_COLUMN_" * lpad(string(i), 4, "0"), SimDataColumn,Column_act_id)
     end
 end
 
